e167cbacfcf6b64047cfebfef197418c82cfa8ed
Trey Causey <trey.causey@gmail.com>
2014-05-18 12:11:33 -0700

    Fixed typos

--- ranking/chapter1.py
+++ ranking/chapter1.py
@@ -19,6 +19,9 @@ games['loser'] = games.apply(lambda x: x.h if x.ptsh < x.ptsv
                               else x.v, axis=1)
 num_games = games.week.max() - 1
 
+# n teams (32)
+# m games (256)
+
 teams = sorted(games.h.unique())
 team_ids = {team: i for i, team in enumerate(teams)}
 game_matrix = np.eye(len(teams)) * num_games # M in Massey
@@ -30,6 +33,7 @@ for w, l in games[['winner', 'loser']].itertuples(index=False):
 
 team_df = pd.DataFrame(index=teams, columns=teams, data=game_matrix)
 
+# Will need this later
 points_for = dict.fromkeys(teams, 0)
 points_against = dict.fromkeys(teams, 0)
 
@@ -51,6 +55,32 @@ point_diff = team_points.point_diff.values # p in Massey
 massey_ratings = np.linalg.solve(game_matrix, point_diff)
 team_points['massey_ratings'] = massey_ratings
 team_points.sort('massey_ratings', ascending=False)
+
+# Massey's Method
+# masseyratings.com
+# Part of the old BCS
+# least squares method:
+# r_i - r_j = y_k
+# where y_k is MoV for game k and r_i and r_j are ratings
+# of teams i and j
+# Equation for every game k which creates m linear equations
+# and n unknowns
+# Xr = y
+# Each row of coefficient matrix X is all 0s except 1 in i
+# and -1 in j meaning i beat j in that game
+# solution is X'Xr = X'y 
+# M = X'X where Mij is the # of games played by team i and 
+# off diagonal element Mij for i != j is negation of # of games
+# played by team i against team j
+# Becomes Mr = p where M is nxn matrix, r is n x 1 vector
+# of unknown ratings and p n x 1 is cumulative point diffs
+
+# Overall rating r_i = o_i + d_i
+# overall pt_diff can be decomposed into f - a
+# coefficient matrix M can be decomposed into T - P
+# where T is diagonal matrix holding total # of games played
+# by each team and P is off diagonal matrix holding
+# number of pairwise matchups
 
 t_matrix = np.diag(np.repeat(num_games, len(teams)))
 
