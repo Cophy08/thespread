03c59d956f86daa9284e1a93e8a4f39e44f8c97e
Trey Causey <trey.causey@gmail.com>
2014-05-18 17:44:39 -0700

    Colley method

--- ranking/chapter2.py
+++ ranking/chapter2.py
@@ -1,5 +1,24 @@
 # Who's #1? The Science of Rating and Ranking
-# Chapter 1
+# Chapter 3
+# Colley's Methods
+
+# based on winning percentage
+# team i rating r_i = w_i / t_i (wins / games played)
+
+# ties in ratings are a problem
+# strength of opponent not factored in
+# undefined at beginning of season
+
+# r_i = (1 + w_i) / (2 + t_i)
+# Will be .5 when the season starts
+# Laplace's rule
+# Strength of Schedule hidden within
+# Because everyone starts at 1/2, points added to ratings are zero-sum
+# Cr = b where r is unknown ratings and b is rhs vector equal to
+# b_i = 1 + 1/2 * (w_i - l_i) and C is Collecy coefficient matrix
+# C_ij = {2 + t_i if i == j
+#        {-n_ij   if i != j 
+# where n_ij is number of times teams i and j played each other
 
 import numpy as np
 import pandas as pd
@@ -16,41 +35,32 @@ games['winner'] = games.apply(lambda x: x.h if x.ptsh > x.ptsv
 games['loser'] = games.apply(lambda x: x.h if x.ptsh < x.ptsv
                               else x.v, axis=1)
 num_games = games.week.max() - 1
-
-# n teams (32)
-# m games (256)
-
 teams = sorted(games.h.unique())
 team_ids = {team: i for i, team in enumerate(teams)}
-game_matrix = np.eye(len(teams)) * num_games # M in Massey
+wins = dict(games.groupby('winner')['winner'].count())
+losses =  dict(games.groupby('loser')['loser'].count())
+
+b_vector = [(1 + .5 * (wins[team] - losses[team])) for team in teams]
+
+game_matrix = np.eye(len(teams)) * (2 + num_games) # C in Colley
 
 # Credit to Sean Taylor for an easy way to create this matrix
 for w, l in games[['winner', 'loser']].itertuples(index=False):
     game_matrix[team_ids[w], team_ids[l]] -= 1
     game_matrix[team_ids[l], team_ids[w]] -= 1
 
-team_df = pd.DataFrame(index=teams, columns=teams, data=game_matrix)
+colley_ratings = np.linalg.solve(game_matrix, b_vector)
 
-# Will need this later
-points_for = dict.fromkeys(teams, 0)
-points_against = dict.fromkeys(teams, 0)
+team_ratings = pd.DataFrame(index=teams)
+team_ratings['colley_ratings'] = colley_ratings
+team_ratings['win_pct'] = [wins[team] / float(wins[team] + 
+                                              losses[team])
+                           for team in teams]
+team_ratings.sort('colley_ratings', ascending=False)
+# DEN falls to 3rd (scores not taken into account)
+# CAR climbs to 2nd, played NFC West, Saints, Patriots
+# No offense / defense
 
-for team in teams:
-    home_points = games[games.h == team]['ptsh'].sum()
-    away_points = games[games.v == team]['ptsv'].sum()
-    points_for[team] = home_points + away_points
-    home_conceded = games[games.h == team]['ptsv'].sum()
-    away_conceded = games[games.v == team]['ptsh'].sum()
-    points_against[team] = home_conceded + away_conceded
-
-team_points = pd.DataFrame(index=teams)
-team_points['points_for'] = pd.Series(points_for)
-team_points['points_against'] = pd.Series(points_against)
-team_points['point_diff'] = team_points.points_for - team_points.points_against
-
-point_diff = team_points.point_diff.values # p in Massey
-
-massey_ratings = np.linalg.solve(game_matrix, point_diff)
 team_points['massey_ratings'] = massey_ratings
 team_points.sort('massey_ratings', ascending=False)
 
