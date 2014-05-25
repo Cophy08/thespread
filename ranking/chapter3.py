52810f3cd53d823cbf1ef0af995c59bede92b6bb
Trey Causey <trey.causey@gmail.com>
2014-05-25 14:42:38 -0700

    Wrapped ratings in function, added examination over time.

--- ranking/chapter3.py
+++ ranking/chapter3.py
@@ -1,69 +1,111 @@
-# Who's #1? The Science of Rating and Ranking
+# Who's #2? The Science of Rating and Ranking
 # Chapter 3
 # Colley's Methods
 
 import numpy as np
 import pandas as pd
+import statsmodels.api as sm
 
-path = '/home/trey/Downloads/nfl'
+def colley_rankings(games, year, massey=False):
+    games = games[games.seas == year]    
+    games = games[games.week <= 17]
+    games['winner'] = games.apply(lambda x: x.h if x.ptsh > x.ptsv
+                                  else x.v, axis=1)
+    games['loser'] = games.apply(lambda x: x.h if x.ptsh < x.ptsv
+                                  else x.v, axis=1)
+
+    num_games = games.week.max() - 1
+
+    wins = dict(games.groupby('winner')['winner'].count())
+    losses =  dict(games.groupby('loser')['loser'].count())
+
+    # Note use of dict.get() method here -- otherwise
+    # New England's 2007 undefeated season causes an error
+    b_vector = [(1 + .5 * (wins.get(team, 0) - losses.get(team, 0))) 
+                for team in teams]
+
+    game_matrix = np.eye(len(teams)) * (2 + num_games) # C in Colley
+
+    # Credit to Sean Taylor for an easy way to create this matrix
+    
+    for w, l in games[['winner', 'loser']].itertuples(index=False):
+        game_matrix[team_ids[w], team_ids[l]] -= 1
+        game_matrix[team_ids[l], team_ids[w]] -= 1
+
+    colley_ratings = np.linalg.solve(game_matrix, b_vector)
+
+    team_ratings = pd.DataFrame(index=teams)
+    team_ratings['colley_ratings'] = colley_ratings
+
+    if massey:
+        points_for = dict.fromkeys(teams, 0)
+        points_against = dict.fromkeys(teams, 0)
+
+        for team in teams:
+            home_points = games[games.h == team]['ptsh'].sum()
+            away_points = games[games.v == team]['ptsv'].sum()
+            points_for[team] = home_points + away_points
+            home_conceded = games[games.h == team]['ptsv'].sum()
+            away_conceded = games[games.v == team]['ptsh'].sum()
+            points_against[team] = home_conceded + away_conceded
+
+        team_points = pd.DataFrame(index=teams)
+        team_points['points_for'] = pd.Series(points_for)
+        team_points['points_against'] = pd.Series(points_against)
+        team_points['point_diff'] = (team_points.points_for - 
+                                     team_points.points_against)
+
+        colley_massey_ratings = np.linalg.solve(game_matrix, 
+                                     team_points.point_diff.values)
+
+        team_ratings['colley_massey_ratings'] = colley_massey_ratings 
+    
+    return team_ratings
+
+path = '/home/trey/Downloads/nfl/'
 
 games = pd.read_csv('%s/GAMES.csv' % path)
 games.columns = [c.lower() for c in games.columns.values]
-games = games[games.seas == 2013]
-# games = games[games.ptsv != games.ptsh]
-games = games[games.week <= 17]
-games['winner'] = games.apply(lambda x: x.h if x.ptsh > x.ptsv
-                              else x.v, axis=1)
-games['loser'] = games.apply(lambda x: x.h if x.ptsh < x.ptsv
-                              else x.v, axis=1)
-num_games = games.week.max() - 1
 teams = sorted(games.h.unique())
 team_ids = {team: i for i, team in enumerate(teams)}
-wins = dict(games.groupby('winner')['winner'].count())
-losses =  dict(games.groupby('loser')['loser'].count())
+seasons = [seas for seas in games.seas.unique()
+           if seas >= 2002]
 
-b_vector = [(1 + .5 * (wins[team] - losses[team])) for team in teams]
+ranking_df = pd.DataFrame(index=teams,
+                          columns=seasons)
 
-game_matrix = np.eye(len(teams)) * (2 + num_games) # C in Colley
+for season in seasons:
+    ranking_df[season] = colley_rankings(games, season)
 
-# Credit to Sean Taylor for an easy way to create this matrix
-for w, l in games[['winner', 'loser']].itertuples(index=False):
-    game_matrix[team_ids[w], team_ids[l]] -= 1
-    game_matrix[team_ids[l], team_ids[w]] -= 1
+def largest_change(s):
+    diff = (s - s.shift())[1:]
+    if abs(min(diff)) >= max(diff):
+        return min(diff)
+    else:
+        return max(diff)
 
-colley_ratings = np.linalg.solve(game_matrix, b_vector)
+ranking_df['max_change'] = ranking_df.apply(largest_change, axis=1)
+ranking_df['max_change'].describe()
 
-team_ratings = pd.DataFrame(index=teams)
-team_ratings['colley_ratings'] = colley_ratings
-team_ratings['win_pct'] = [wins[team] / float(wins[team] + 
-                                              losses[team])
-                           for team in teams]
-team_ratings.sort('colley_ratings', ascending=False)
+ranking_df[ranking_df.max_change == ranking_df.max_change.max()]
 
-# DEN falls to 3rd (scores not taken into account)
-# CAR climbs to 2nd, played NFC West, Saints, Patriots
-# No offense / defense
+ranking_df[ranking_df.max_change == ranking_df.max_change.min()]
 
-points_for = dict.fromkeys(teams, 0)
-points_against = dict.fromkeys(teams, 0)
+# Using Colley-Massey
 
-for team in teams:
-    home_points = games[games.h == team]['ptsh'].sum()
-    away_points = games[games.v == team]['ptsv'].sum()
-    points_for[team] = home_points + away_points
-    home_conceded = games[games.h == team]['ptsv'].sum()
-    away_conceded = games[games.v == team]['ptsh'].sum()
-    points_against[team] = home_conceded + away_conceded
+mov_ranking_df = pd.DataFrame(index=teams,
+                          columns=seasons)
 
-team_points = pd.DataFrame(index=teams)
-team_points['points_for'] = pd.Series(points_for)
-team_points['points_against'] = pd.Series(points_against)
-team_points['point_diff'] = team_points.points_for - team_points.points_against
+for season in seasons:
+    mov_ranking_df[season] = colley_rankings(games, 
+                                             season, 
+                                             massey=True)['colley_massey_ratings']
 
-colley_massey_ratings = np.linalg.solve(game_matrix, 
-                                 team_points.point_diff.values)
+mov_ranking_df['max_change'] = mov_ranking_df.apply(largest_change, axis=1)
+mov_ranking_df['max_change'].describe()
 
-team_ratings['colley_massey_ratings'] = colley_massey_ratings
-team_ratings.sort('colley_massey_ratings', ascending=False)
+mov_ranking_df[mov_ranking_df.max_change == mov_ranking_df.max_change.max()]
+
+mov_ranking_df[mov_ranking_df.max_change == mov_ranking_df.max_change.min()]
 
 
